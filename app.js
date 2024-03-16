64e419d09036016dda3a9d5ef8fd30ab13d190d2
aliza <ak6614462@gmail.com>
2024-03-17 01:14:25 +0500

    ass 17

---
+++ app.js
@@ -0,0 +1,174 @@
+// Question 1
+// var arr=[
+//     ["abc",100,"aliza"]
+//     ["xyz",80]
+// ]
+
+// Q2
+// var matrix=[
+//     [0, 1, 2, 3],
+//     [1, 0, 1, 2],
+//     [2, 1, 0, 1],
+
+// ]
+// console.log(matrix)
+
+// Q3
+// for(var i=1; i<=10;i++){
+//     document.write(i+"<br>")
+// }
+
+// Q4
+// var number=prompt("Enter a number to show its multiplication table:")
+// var length=prompt("Enter length")
+// for (var i=1;i<=length;i++){
+//     document.write(number,"x",i,"=",number*i,"<br>")
+// }
+
+// Q5
+// var fruits = ["apple", "banana", "mango", "orange”",
+//     "strawberry"]
+// for (var i=0;i<fruits.length;i++){
+//     document.write("Element at index ",i,"  ", fruits[i],"<br>")
+// }   
+
+// Q6
+// document.write("Counting","<br>")
+// for(var i=1;i<=10;i++){
+//     document.write(i,",")
+// }
+// document.write("<br>")
+
+// document.write("Reverse counting","<br>")
+// for (var j=10;j>=1;j--){
+//     document.write(j,",")
+// }
+
+// document.write("Even","<br>")
+// for (var k=2; )
+
+// Q7
+// var searchitem=prompt("Enter item you want to search: ")
+// var a = ["cake", "apple pie", "cookie", "chips", "patties"]
+// var flag=false
+// for (var i=0;i<a.length;i++){
+//     if (searchitem===a[i]){
+//         flag="true"
+//     }
+
+// }
+// if (flag==="true"){
+//     console.log("Found it...")
+// }
+// else{
+//     console.log("Not Found...")
+// }
+
+// Q8
+// var a = [24, 53, 78, 91, 12]
+// var max=a[0]
+// for(var i=0;i<a.length;i++){
+//     if (a[i]>max){
+//         max=a[i]
+//     }
+// console.log(max)    
+// }
+
+// Q9
+// var a = [24, 53, 78, 91, 12]
+// var min=a[0]
+// for(var i=0;i<a.length;i++){
+//     if (a[i]<max){
+//         min=a[i]
+//     }
+// console.log(min)    
+// }
+
+
+// Q10
+// for (x=1; x <= 100; x++){
+//     if( x % 5 == 0 ){
+//         console.log(x)
+//     }
+// }    
+
+
+// Q1
+// var password=prompt("enter password: ")
+// if (password!=""){
+//     if(password.length<=5){
+//         alert("Password should be greater than 5")
+//     }
+//     else{
+//         alert("OK")
+//     }
+// }
+
+// Q2
+// if (a === 1) {
+//      if (c === "Max") {
+//     alert("OK");
+//     }
+//     }
+
+// Q3
+// var a=2
+// var b=2
+// if( a===b){
+//     if(a<=b){
+//         alert("Both conditions passed:")
+//     }
+// }
+
+// chapter 15
+// Q1
+// var array=[];
+
+// Q2
+// var array=["aliza"]
+
+// Q3
+// var alphabet=["h", "i", "j", "k"]
+// document.write(alphabet[2])
+
+// Q4
+// var alphabet=["h", "i", "j", "k", "l", "m", "n", "o"]
+// console.log(alphabet.length)
+
+// Q5
+// var arr=["aliza"]
+// arr[1]="shanze"
+// alert(arr[1])
+
+// CHAPTER 16
+// var arr=["aliza"]
+// arr.push("khan")
+// alert(arr[arr.length-1])
+
+// Q2
+// var Alphabet = ["h", "i", "j", "k"];
+
+ // Remove the last element from the array Alphabet
+// Alphabet.pop();
+
+// Q3
+// var Alphabet = ["h", "i", "j", "k"];
+// Alphabet.push(5)
+
+// CHAPTER 16
+// Q1
+// var sizes = ["S", "M", "XL", "XXL", "XXXL"];
+// sizes.shift()
+// console.log(sizes)
+
+// Q2
+// var sizes = ["S", "M", "XL", "XXL", "XXXL"];
+// sizes.unshift(3,4,5)
+// console.log(sizes)
+
+// Q3
+
+
+
+
+
